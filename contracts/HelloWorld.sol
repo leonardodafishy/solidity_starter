3f240f7f7576f49ca0bc045bb680e552e84fd193
Miguel Fesalbon <70272939+leonardodafishy@users.noreply.github.com>
2023-10-10 18:52:59 +0800

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,6 +3,34 @@
 // compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
 pragma solidity ^0.8.17;
 
-contract HelloWorld {
-    string public greet = "Hello World!";
+contract ExceptionExample {
+    uint256 public balance;
+
+    constructor(uint256 initialBalance) {
+        balance = initialBalance;
+    }
+
+    function deposit(uint256 amount) public {
+        // Use require to check a condition and revert if it fails
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Use assert to check an internal inconsistency and revert if it fails
+        assert(balance + amount >= balance);
+
+        // Update the balance
+        balance += amount;
+    }
+
+    function withdraw(uint256 amount) public {
+        // Use require to check a condition and revert if it fails
+        require(balance >= amount, "Insufficient balance");
+
+        // Update the balance
+        balance -= amount;
+    }
+
+    function triggerRevert() public pure {
+        // Use revert to explicitly trigger a revert
+        revert("This transaction intentionally reverted");
+    }
 }
